4ef391cbefdc07f051f0a35fda045af560f36cf9
JekaGrib <1234jane@mail.ru>
2022-01-28 15:14:24 +0300

    wall-werror through TestVk i.e. all programm

--- test/TestVk.hs
+++ test/TestVk.hs
@@ -1,5 +1,5 @@
---{-# OPTIONS_GHC -Werror #-}
---{-# OPTIONS_GHC  -Wall  #-}
+{-# OPTIONS_GHC -Werror #-}
+{-# OPTIONS_GHC  -Wall  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module TestVk where
@@ -8,7 +8,7 @@ import           Test.Hspec (it,shouldThrow,shouldBe,describe,hspec)
 import           Vk.App (Handle(..),runServ,getServer)
 import           Vk.Logger (LogHandle(..),Priority(..),LogConfig(..))
 import           Vk.Api.Response                (ServerInfo(..))
-import           Control.Monad.State
+import           Control.Monad.State (StateT(..),evalStateT,execStateT)
 import Vk.Types
 import Vk.Oops (VKBotException(..))
 import Vk.Conf (Config(..))
@@ -40,16 +40,30 @@ logTest0 :: Priority -> String -> StateT [MockAction] IO ()
 logTest0 prio str = StateT $ \s -> 
     return (() , LOGMSG prio str : s)
 
+config1 :: Config 
 config1 = Config { cStartN = 2 , cBotToken = "ABC123" , cHelpMsg = "Hello" , cRepeatQ = "Why?", cGroupId = 321, cPriority = DEBUG}
+
+handleLog1,handleLog0 :: LogHandle (StateT [MockAction] IO)
 handleLog1 = LogHandle (LogConfig DEBUG) logTest
 handleLog0 = LogHandle (LogConfig DEBUG) logTest0
+
+handle1 ::  Handle (StateT [MockAction] IO)
 handle1 = Handle { hConf = config1,
                    hLog = handleLog1,
                    getLongPollServer = getServerTest json1,
                    getUpdates = getUpdatesTest json2,
                    sendMsg = sendMsgTest json4,
-                   sendKeyb = sendKeybTest json4}
+                   sendKeyb = sendKeybTest json4,
+                   getPhotoServer = \_ -> return json1,
+                   loadPhotoToServ = \_ _ _-> return json1,
+                   savePhotoOnServ = \_ -> return json1,
+                   getDocServer = \_ _ -> return json1,
+                   loadDocToServ =  \_ _ _ _ -> return json1,
+                   saveDocOnServ = \_ _ -> return json1,
+                   goToUrl = \_ -> return jsonA
+                   }
 
+handle0,handle2,handle3,handle4,handle5,handle6,handle7 :: Handle (StateT [MockAction] IO)
 handle0  = handle1 { hLog = handleLog0 }
 handle2 = handle1 {getUpdates = getUpdatesTest json3, hLog = handleLog0}
 handle3 = handle1 {getLongPollServer = getServerTest json5}
@@ -58,6 +72,7 @@ handle5 = handle1 {getUpdates = getUpdatesTest json7, hLog = handleLog0}
 handle6 = handle1 {getUpdates = getUpdatesTest json5}
 handle7 = handle1 {getUpdates = getUpdatesTest json6}
 
+initialDB1 :: MapUserN
 initialDB1 = Map.fromList []
 
 testVk :: IO ()
@@ -73,8 +88,8 @@ testVk = hspec $ do
   
   describe "(getServer >>= runServ)" $ do
     it "work with empty update list" $ do
-      state <- execStateT (evalStateT (getServer handle0 >> runServ handle0) (ServerInfo "A" "A" "1",initialDB1) ) []
-      reverse state `shouldBe`
+      actions <- execStateT (evalStateT (getServer handle0 >> runServ handle0) (ServerInfo "A" "A" "1",initialDB1) ) []
+      reverse actions `shouldBe`
         [LOGMSG DEBUG "Send request to getLongPollServer: https://api.vk.com/method/groups.getLongPollServer?group_id=321&access_token=ABC123&v=5.103\n", 
         GOTSERVER,
         LOGMSG DEBUG $ "Get response: " ++ show json1 ++ "\n",
@@ -85,8 +100,8 @@ testVk = hspec $ do
         LOGMSG INFO "No new updates\n"]
 
     it "work with singleton update list with text msg" $ do
-      state <- execStateT (evalStateT (getServer handle2 >> runServ handle2) (ServerInfo "A" "A" "1",initialDB1) ) []
-      reverse state `shouldBe`
+      actions <- execStateT (evalStateT (getServer handle2 >> runServ handle2) (ServerInfo "A" "A" "1",initialDB1) ) []
+      reverse actions `shouldBe`
         [LOGMSG DEBUG "Send request to getLongPollServer: https://api.vk.com/method/groups.getLongPollServer?group_id=321&access_token=ABC123&v=5.103\n",
         GOTSERVER,
         LOGMSG DEBUG "Get response: \"{\\\"response\\\":{\\\"key\\\":\\\"912481cc91cb3b0e119b9be5c75b383d6887438f\\\",\\\"server\\\":\\\"https:\\\\/\\\\/lp.vk.com\\\\/wh000\\\",\\\"ts\\\":\\\"289\\\"}}\"\n",
@@ -104,8 +119,8 @@ testVk = hspec $ do
           LOGMSG INFO "Msg \"love\" was sent to user 123\n"])
     
     it "work with singleton update list with sticker msg " $ do
-      state <- execStateT (evalStateT (getServer handle5 >> runServ handle5) (ServerInfo "A" "A" "1",initialDB1) ) []
-      reverse state `shouldBe`
+      actions <- execStateT (evalStateT (getServer handle5 >> runServ handle5) (ServerInfo "A" "A" "1",initialDB1) ) []
+      reverse actions `shouldBe`
         [LOGMSG DEBUG "Send request to getLongPollServer: https://api.vk.com/method/groups.getLongPollServer?group_id=321&access_token=ABC123&v=5.103\n", 
         GOTSERVER,
         LOGMSG DEBUG $ "Get response: " ++ show json1 ++ "\n",
@@ -131,6 +146,7 @@ testVk = hspec $ do
       evalStateT (evalStateT (getServer handle7 >> runServ handle7) (ServerInfo "A" "A" "1",initialDB1) ) []
         `shouldThrow` ( == (CheckGetUpdatesResponseException $ "UNKNOWN RESPONSE:\n" ++ show json6))
 
+json1,json2,json3,json4,json5,json6,json7 :: Response
 json1 = "{\"response\":{\"key\":\"912481cc91cb3b0e119b9be5c75b383d6887438f\",\"server\":\"https:\\/\\/lp.vk.com\\/wh000\",\"ts\":\"289\"}}"
 json2 = "{\"ts\":\"289\",\"updates\":[]}\r\n"
 json3 = "{\"ts\":\"290\",\"updates\":[{\"type\":\"message_new\",\"object\":{\"date\":1594911394,\"from_id\":123,\"id\":597,\"out\":0,\"peer_id\":16063921,\"text\":\"love\",\"conversation_message_id\":562,\"fwd_messages\":[],\"important\":false,\"random_id\":0,\"attachments\":[],\"is_hidden\":false},\"group_id\":194952914,\"event_id\":\"35ec397e45dfe993d365912ea32be41be5e77a0c\"}]}\r\n"
@@ -139,4 +155,5 @@ json5 = "{\"error\":{\"error_code\":5,\"error_msg\":\"User authorization failed:
 json6 = "lalala"
 json7 = "{\"ts\":\"304\",\"updates\":[{\"type\":\"message_new\",\"object\":{\"date\":1594932378,\"from_id\":16063921,\"id\":629,\"out\":0,\"peer_id\":16063921,\"text\":\"\",\"conversation_message_id\":594,\"fwd_messages\":[],\"important\":false,\"random_id\":0,\"attachments\":[{\"type\":\"sticker\",\"sticker\":{\"product_id\":279,\"sticker_id\":9014,\"images\":[{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-64\",\"width\":64,\"height\":64},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-128\",\"width\":128,\"height\":128},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-256\",\"width\":256,\"height\":256},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-352\",\"width\":352,\"height\":352},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-512\",\"width\":512,\"height\":512}],\"images_with_background\":[{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-64b\",\"width\":64,\"height\":64},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-128b\",\"width\":128,\"height\":128},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-256b\",\"width\":256,\"height\":256},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-352b\",\"width\":352,\"height\":352},{\"url\":\"https:\\/\\/vk.com\\/sticker\\/1-9014-512b\",\"width\":512,\"height\":512}]}}],\"is_hidden\":false},\"group_id\":194952914,\"event_id\":\"a3d68972637b90446ac3be5a171d923fa0f10f31\"}]}\r\n"
 
-
+jsonA :: ResponseS
+jsonA = "{\"response\":{\"key\":\"912481cc91cb3b0e119b9be5c75b383d6887438f\",\"server\":\"https:\\/\\/lp.vk.com\\/wh000\",\"ts\":\"289\"}}"
