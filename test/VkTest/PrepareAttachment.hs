8f9b916823c5a7a4e8cf3a2454335dc53b34ccf1
JekaGrib <1234jane@mail.ru>
2022-02-17 14:09:41 +0300

    add tests

--- test/VkTest/PrepareAttachment.hs
+++ test/VkTest/PrepareAttachment.hs
@@ -1,17 +1,16 @@
-{-{-# OPTIONS_GHC -Werror #-}
-{-# OPTIONS_GHC  -Wall  #-}-}
+{-# OPTIONS_GHC -Werror #-}
+{-# OPTIONS_GHC  -Wall  #-}
 {-# LANGUAGE OverloadedStrings #-} 
 
 module VkTest.PrepareAttachment where
 
 import Vk.App.PrepareAttachment
 import VkTest.PrepareAttachment.Handlers
-import VkTest.PrepareAttachment.ResponseExample
 import VkTest.PrepareAttachment.Types
-import Control.Monad.State (execStateT,runStateT)
+import VkTest.PrepareAttachment.Oops
+import Control.Monad.State (runStateT,evalStateT)
 import Control.Monad.Except (runExceptT)
 import Vk.Logger (Priority(..))
-import Vk.Types
 import Vk.Api.Response (Size(..),Photo(..),Attachment(..),Doc(..),Audio(..))
 import Test.Hspec (describe, hspec, it, shouldBe, shouldThrow)
 import Vk.Api.Response (LoadPhotoResp(..),LoadDocResp(..),DocInfo(..))
@@ -67,6 +66,27 @@ testVkPrAtt =
           ,LOG DEBUG]
         docInfo `shouldBe`
           DocInfo 25 50
+      it "throw CheckGetUploadServerResponseException on unknown getUploadServer response" $ do
+        runStateT (getPhotoDocInfo handle6 5 "http://pic.jpg") [] `shouldThrow`
+          isCheckGetUploadServerResponseException
+      it "throw CheckLoadToServResponseException on unknown loadToServer response" $ do
+        runStateT (getPhotoDocInfo handle7 5 "http://pic.jpg") [] `shouldThrow`
+          isCheckLoadToServResponseException
+      it "throw CheckSaveOnServResponseException on unknown saveOnServer response" $ do
+        runStateT (getPhotoDocInfo handle8 5 "http://pic.jpg") [] `shouldThrow`
+          isCheckSaveOnServResponseException
+      it "throw GetUploadServerException on getUploadServer HttpException" $ do
+        runStateT (getPhotoDocInfo handle12 5 "http://pic.jpg") [] `shouldThrow`
+          isGetUploadServerException
+      it "throw LoadToServException on loadToServer HttpException" $ do
+        runStateT (getPhotoDocInfo handle13 5 "http://pic.jpg") [] `shouldThrow`
+          isLoadToServException
+      it "throw SaveOnServException on saveOnServer HttpException" $ do
+        runStateT (getPhotoDocInfo handle14 5 "http://pic.jpg") [] `shouldThrow`
+          isSaveOnServException
+      it "throw GoToUrlException on goToUrl HttpException" $ do
+        runStateT (getPhotoDocInfo handle15 5 "http://pic.jpg") [] `shouldThrow`
+          isGoToUrlException
     describe "getDocInfo" $ do
       it "work with photo attachment with user 6 doc_url=\"http://doc\"" $ do
         (docInfo,actions) <- 
@@ -83,6 +103,27 @@ testVkPrAtt =
           ,LOG DEBUG]
         docInfo `shouldBe`
           DocInfo 25 50
+      it "throw CheckGetUploadServerResponseException on unknown getUploadServer response" $ do
+        runStateT (getDocInfo handle9 6 (Doc "http://doc" "hs" "MyDoc")) [] `shouldThrow`
+          isCheckGetUploadServerResponseException
+      it "throw CheckLoadToServResponseException on unknown loadToServer response" $ do
+        runStateT (getDocInfo handle10 6 (Doc "http://doc" "hs" "MyDoc")) [] `shouldThrow`
+          isCheckLoadToServResponseException
+      it "throw CheckSaveOnServResponseException on unknown saveOnServer response" $ do
+        runStateT (getDocInfo handle11 6 (Doc "http://doc" "hs" "MyDoc")) [] `shouldThrow`
+          isCheckSaveOnServResponseException
+      it "throw GetUploadServerException on getUploadServer HttpException" $ do
+        runStateT (getDocInfo handle16 6 (Doc "http://doc" "hs" "MyDoc")) [] `shouldThrow`
+          isGetUploadServerException
+      it "throw LoadToServException on loadToServer HttpException" $ do
+        runStateT (getDocInfo handle17 6 (Doc "http://doc" "hs" "MyDoc")) [] `shouldThrow`
+          isLoadToServException
+      it "throw SaveOnServException on saveOnServer HttpException" $ do
+        runStateT (getDocInfo handle18 6 (Doc "http://doc" "hs" "MyDoc")) [] `shouldThrow`
+          isSaveOnServException
+      it "throw GoToUrlException on goToUrl HttpException" $ do
+        runStateT (getDocInfo handle15 6 (Doc "http://doc" "hs" "MyDoc"))[] `shouldThrow`
+          isGoToUrlException
     describe "getAudioMsgDocInfo" $ do
       it "work with audioMsg attachment with user 4 doc_url=\"http://doc\"" $ do
         (docInfo,actions) <- 
@@ -97,17 +138,34 @@ testVkPrAtt =
           ,LOG DEBUG]
         docInfo `shouldBe`
           DocInfo 25 50
+      it "throw CheckGetUploadServerResponseException on unknown getUploadServer response" $ do
+        runStateT (getAudioMsgDocInfo handle9 4 (Audio "http://doc")) [] `shouldThrow`
+          isCheckGetUploadServerResponseException
+      it "throw CheckLoadToServResponseException on unknown loadToServer response" $ do
+        runStateT (getAudioMsgDocInfo handle10 4 (Audio "http://doc")) [] `shouldThrow`
+          isCheckLoadToServResponseException
+      it "throw CheckSaveOnServResponseException on unknown saveOnServer response" $ do
+        runStateT (getAudioMsgDocInfo handle11 4 (Audio "http://doc")) [] `shouldThrow`
+          isCheckSaveOnServResponseException
+      it "throw GetUploadServerException on getUploadServer HttpException" $ do
+        runStateT (getAudioMsgDocInfo handle16 4 (Audio "http://doc")) [] `shouldThrow`
+          isGetUploadServerException
+      it "throw LoadToServException on loadToServer HttpException" $ do
+        runStateT (getAudioMsgDocInfo handle17 4 (Audio "http://doc")) [] `shouldThrow`
+          isLoadToServException
+      it "throw SaveOnServException on saveOnServer HttpException" $ do
+        runStateT (getAudioMsgDocInfo handle18 4 (Audio "http://doc")) [] `shouldThrow`
+          isSaveOnServException
+      it "throw GoToUrlException on goToUrl HttpException" $ do
+        runStateT (getAudioMsgDocInfo handle15 4 (Audio "http://doc"))[] `shouldThrow`
+          isGoToUrlException
     describe "getAttachmentString" $ do
       it "work with photo attachment with user 5" $ do
-        actions <- 
-          execStateT 
+        attStr <- 
+          evalStateT
            (runExceptT $ (getAttachmentString handle1 5 (PhotoAttachment (Photo [Size 3 3 "http://pic.jpg"])))) 
            []
-        reverse actions `shouldBe`
-          [GOTPhotoSERVER 5
-          ,LOG DEBUG
-          ,GOTOURL "http://pic.jpg"
-          ,LOADPhotoTOSERV "http://toLoadPic" "http://pic.jpg" "anyPhoto"
-          ,LOG DEBUG
-          ,SAVEPhotoONSERV (LoadPhotoResp  24 "anyHash" "anyPhotoSring")
-          ,LOG DEBUG]
+        attStr `shouldBe`
+          Right "photo50_25"
+
+        
---
+++ test/VkTest/PrepareAttachment/Oops.hs
@@ -0,0 +1,34 @@
+{-# OPTIONS_GHC -Werror #-}
+{-# OPTIONS_GHC  -Wall  #-}
+
+module VkTest.PrepareAttachment.Oops where
+
+import Vk.Oops
+
+isGetUploadServerException :: VKBotException -> Bool
+isGetUploadServerException (PrependAttachmetToSendException GetUploadServerException{}) = True
+isGetUploadServerException _ = False
+
+isCheckGetUploadServerResponseException :: VKBotException -> Bool
+isCheckGetUploadServerResponseException (PrependAttachmetToSendException CheckGetUploadServerResponseException{}) = True
+isCheckGetUploadServerResponseException _ = False
+
+isLoadToServException :: VKBotException -> Bool
+isLoadToServException (PrependAttachmetToSendException LoadToServException{}) = True
+isLoadToServException _ = False
+
+isCheckLoadToServResponseException :: VKBotException -> Bool
+isCheckLoadToServResponseException (PrependAttachmetToSendException CheckLoadToServResponseException{}) = True
+isCheckLoadToServResponseException _ = False
+
+isSaveOnServException :: VKBotException -> Bool
+isSaveOnServException (PrependAttachmetToSendException SaveOnServException{}) = True
+isSaveOnServException _ = False
+
+isCheckSaveOnServResponseException :: VKBotException -> Bool
+isCheckSaveOnServResponseException (PrependAttachmetToSendException CheckSaveOnServResponseException{}) = True
+isCheckSaveOnServResponseException _ = False
+
+isGoToUrlException :: VKBotException -> Bool
+isGoToUrlException (PrependAttachmetToSendException GoToUrlException{}) = True
+isGoToUrlException _ = False
